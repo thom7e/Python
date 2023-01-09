e5ffb957807a55950ac8abd80211dbffad9cc6ad
thom7e <thomas.kurfiss@gmail.com>
2023-01-09 16:53:36 +0100

    newversion

--- AOC/2016/day-7.py
+++ AOC/2016/day-7.py
@@ -1,35 +1,79 @@
-import hashlib
+import re
+with open("day-7.in") as d:
+    inp = d.read().splitlines()
 
-input = "ugkcyxxp"
-def part_1():
-    pwd = []
-    counter = 0
-    for number in range(0,99999999):
-        string = f"{input}{number}"
-        result = hashlib.md5(string.encode())
-        if result.hexdigest()[0:5] == "00000":
-            pwd.append(result.hexdigest()[5])
-            counter += 1
-            if counter == 8:
-                return "".join(pwd)
+counter_p1 = 0
+counter_p2 = 0
 
-def part_2():
-    pwd = []
-    already_filled = []
-    for number in range(0,99999999):
-        string = f"{input}{number}"
-        result = hashlib.md5(string.encode())
-        if result.hexdigest()[0:5] == "00000":
-            if result.hexdigest()[5] in ["0","1","2","3","4","5","6","7"]:
+def part_1(supernet,hypernet):
+    check = []
+    sq = supernet
+    hypernet_seq = hypernet
+    for outsiders in sq:
+        # print(outsiders)
+        for first_ in range(len(outsiders) - 3):
 
-                if not result.hexdigest()[5] in already_filled:
-                    already_filled.append(result.hexdigest()[5])
-                    pwd.append((int(result.hexdigest()[5]),result.hexdigest()[6]))
-                    if len(already_filled) == 8:
-                        pwd_clear = []
-                        for x,y in sorted(pwd):
-                            pwd_clear.append(y)
-                        return "".join(pwd_clear)
+            four_char = outsiders[first_:first_ + 4]
+            # print(four_char)
 
-print("PART I:",part_1())
-print("PART II:",part_2())
+            if four_char[0] == four_char[3] and four_char[1] == four_char[2]:
+                if four_char[0] == four_char[1] or four_char[2] == four_char[3]:
+                    check.append("false")
+                    break
+                else:
+                    check.append("true")
+                    break
+
+    for insiders in hypernet_seq:
+
+        for second_ in range(len(insiders) - 3):
+            four_char_ = insiders[second_:second_ + 4]
+
+            if four_char_[0] == four_char_[-1] and four_char_[1] == four_char_[2]:
+                if four_char_[0] == four_char_[1] or four_char_[2] == four_char_[3]:
+                    break
+                else:
+                    check.append("false")
+                    break
+
+    if not "false" in check and not check == []:
+        return True
+
+def compile_p1(string):
+    pattern = "\W"
+    split = re.split(pattern, string)
+    sq = split[::2]
+    hypernet_seq = split[1::2]
+    return (sq,hypernet_seq)
+
+def part_2(supernet,hypernet):
+    sq = supernet
+    hypernet_seq = hypernet
+    hyper_check_liste = []
+    for insiders in hypernet_seq:
+        for second_ in range(len(insiders) - 2):
+
+            four_char = insiders[second_:second_ + 3]
+            if four_char[0] == four_char[-1]:
+                hyper_check_liste.append(four_char)
+
+    for outsiders in sq:
+        for first_ in range(len(outsiders) - 2):
+
+            four_char = outsiders[first_:first_ + 3]
+            if four_char[0] == four_char[-1]:
+                corresponding = "".join([four_char[1],four_char[0],four_char[1]])
+
+                if corresponding in hyper_check_liste:
+                    return True
+
+for ipv6 in inp:
+    if part_1(compile_p1(ipv6)[0],compile_p1(ipv6)[1]):
+        counter_p1 += 1
+
+    if part_2(compile_p1(ipv6)[0],compile_p1(ipv6)[1]):
+        counter_p2 += 1
+
+
+print("PART I",counter_p1)
+print("PART II",counter_p2)
